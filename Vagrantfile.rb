f69557604803826015e707ddf7c7ac2cd42c6bc2
Yuri <yuri.md13@gmail.com>
2025-01-09 12:11:06 -0300

    fix: typing errors, --advertise-addr syntax fixed and docker swarm join port changed to master node ip

--- Vagrantfile.rb
+++ Vagrantfile.rb
@@ -1,7 +1,4 @@
 Vagrant.configure("2") do |config|
-
-    swarn_token = "SWMTKN-1-23456789abcdefgh0123456789abcdefgh0123456789abcdefgh0123456789abcdefgh"
-
     config.vm.define "master" do |master|
         master.vm.box = "ubuntu/bionic64"
         master.vm.network "private_network", type: "dhcp"
@@ -15,31 +12,27 @@ Vagrant.configure("2") do |config|
         master.vm.provision "shell", inline: <<-SHELL
             sudo apt-get update
             sudo apt-get install -y docker.io
+            swarm_output=$(sudo docker swarm init --advertise-addr eth1)
+            swarm_token=$(echo "$swarm_output" | grep "SWMTKN" | awk '{print $5}')
+            echo "$swarm_token" > /vagrant/swarm-token
+        SHELL
+    end
 
-            sudo docker swarn init --advertise--addr eth1
-
-            echo "#{swarn_token}" > /vagrant/swarn-token
+    ["node01", "node02", "node03"].each do |node_name|
+        config.vm.define node_name do |node|
+            node.vm.box = "ubuntu/bionic64"
+            node.vm.network "private_network", type: "dhcp"
+            node.vm.hostname = node_name
+            node.vm.provider "virtualbox" do |v|
+                v.memory = 1024
+                v.cpus = 1
+            end
+            node.vm.provision "shell", inline: <<-SHELL
+                sudo apt-get update
+                sudo apt-get install -y docker.io
+                swarm_token=$(cat /vagrant/swarm-token)
+                sudo docker swarm join --token $swarm_token 192.168.33.10:2377
             SHELL
         end
-        
-        ["node01", "node02", "node03"].each do |node_name|
-            config.vm.define node_name do |node|
-                node.vm.box = "ubutu/bionic64"
-                node.vm.network "private_network", type: "dhcp"
-                node.vm.hostname = node_name
-                node.vm.provider "virtualbox" do |v|
-                    v.memory = 1024
-                    v.cpus = 1
-                end
-                node.vm.provision "shell", inline: <<-SHELL
-                    sudo apt-get update
-                    sudo apt-get install -y docker.io
-
-                    swarn_token=$(cat /vagrant/swarn-token)
-
-                    sudo docker swarn join --token $swarn_token master:2377
-                SHELL
-            end
-        end
-        
+    end
 end
